e22e951378f570c7de3f0af02790b060781eef36
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-02-02 13:01:19 +0100

    create objects for key-input values and add basic functionality for checking these values

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,73 @@
 const buttons = document.querySelectorAll('.btn');
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const numberOne = [];
+
+const numberValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const operatorValues = ['+', '-', 'X', '/'];
+
+let activeNumber = [];
+let operatorArr = [];
+
+let currentKey = {
+  value: null,
+  type: null,
+};
+
+let previousKey = {
+  value: null,
+  type: null,
+};
+
+let numberOne;
+let numberTwo;
+let operator;
 
 buttons.forEach(button => {
   button.addEventListener('click', event => {
-    if (numbers.includes(event.target.innerText)) {
-      numberOne.push(event.target.innerText);
+    const btnContent = event.target.innerText;
+
+    let btnType;
+
+    if (numberValues.includes(btnContent)) {
+      btnType = 'number';
+    }
+    if (operatorValues.includes(btnContent)) {
+      btnType = 'operator';
     }
 
-    console.log(numberOne);
+    if (currentKey.value != null) {
+      previousKey.value = currentKey.value;
+      previousKey.type = currentKey.type;
+    }
+
+    currentKey.value = btnContent;
+    currentKey.type = btnType;
+
+    if (
+      currentKey.type === 'number' &&
+      (previousKey.type === 'number' || previousKey.type === null)
+    ) {
+      activeNumber.push(currentKey.value);
+    }
+    if (currentKey.type === 'operator' && activeNumber.length != 0) {
+      operatorArr.push(currentKey.value);
+    }
+
+    console.log(previousKey);
+    console.log(currentKey);
+    console.log(activeNumber);
+    console.log(operatorArr);
   });
 });
+
+/*     if (numberValues.includes(btnContent)) {
+      activeNumber.push(btnContent);
+    }
+
+    if (operatorValues.includes(btnContent)) {
+      operatorArr.push(btnContent);
+      numberOne = activeNumber.toString().replaceAll(',', '');
+      activeNumber = [];
+    } 
+    
+      console.log(activeNumber);
+    console.log(numberOne);
+    console.log(operatorArr);*/
